9ee486eb65d77e3f9a0dfea3703f666c72e0a9fb
Björn Gerdau <bjoerngerdau@gmx.de>
2019-05-01 12:04:29 +0200

    test: add tests for option appliers

---
+++ option_test.go
@@ -0,0 +1,37 @@
+package bulk_test
+
+import (
+	bulk "github.com/kernle32dll/httpbulk-go"
+	"net/http"
+	"testing"
+)
+
+// Tests that the ConcurrencyLimit option correctly applies.
+func Test_Option_ConcurrencyLimit(t *testing.T) {
+	// given
+	option := bulk.ConcurrencyLimit(9001)
+	options := &bulk.Options{ConcurrencyLimit: 42}
+
+	// when
+	option(options)
+
+	// then
+	if options.ConcurrencyLimit != 9001 {
+		t.Errorf("concurrencly limit not correctly applied, got %d", options.ConcurrencyLimit)
+	}
+}
+
+// Tests that the Client option correctly applies.
+func Test_Option_Client(t *testing.T) {
+	// given
+	option := bulk.Client(http.DefaultClient)
+	options := &bulk.Options{Client: nil}
+
+	// when
+	option(options)
+
+	// then
+	if options.Client != http.DefaultClient {
+		t.Error("client not correctly applied")
+	}
+}
